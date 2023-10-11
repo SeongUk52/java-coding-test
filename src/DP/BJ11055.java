97a4450f068297a9bb40cc3b541c24522dac9698
정성욱 <rkfka7849@gmail.com>
2023-10-11 15:28:22 +0900

    feat(알고리즘): 백준 11055 DP 최장수열

---
+++ src/DP/BJ11055.java
@@ -0,0 +1,29 @@
+package DP;
+
+import java.util.Scanner;
+
+class Main {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        int max = 0;
+        int[] a = new int[n];
+        int[] dp = new int[n];
+        for(int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            dp[i] = a[i];
+            for (int j = 0; j < i; j++) {
+                if (a[j] < a[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + a[i]);
+                }
+            }
+            if (dp[i] > max) {
+                max = dp[i];
+            }
+        }
+        System.out.println(max);
+    }
+
+}
